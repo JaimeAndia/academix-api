3f781a04766f4f77a5c14d385257712dbe24fa11
JaimeAndia <andiacintorajaime@gmail.com>
2026-03-24 19:05:16 +0100

    Proyecto Spring Boot inicial generado por Spring Initializr

---
+++ src/main/java/com/jaime/academix/AcademixApplication.java
@@ -0,0 +1,13 @@
+package com.jaime.academix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaime/academix/AcademixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaime.academix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
